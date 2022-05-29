1e81a62c34cb157be0b93f70d889280134fe11df
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-05-29 18:45:00 -0300

    Criando Menu lateral

--- Exercicios/contador-numeros/src/App.vue
+++ Exercicios/contador-numeros/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <main>
-    
+  <main class="columns">
+    <div class="column is-one-quarter">
+      <MenuLateral/>
+    </div>
+    <div class="column is-three-quarter">
+
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import MenuLateral from './components/MenuLateral.vue';
 
 export default defineComponent({
-  name: 'App'
+    name: "App",
+    components: { MenuLateral }
 });
 </script>
 
---
+++ Exercicios/contador-numeros/src/components/MenuLateral.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="columns is-justify-content-center">
+        <img src="../assets/verifica.png" alt="Logo Contador Numeros">
+    </header>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue"
+
+export default defineComponent({
+
+})
+</script>
+
+<style>
+header {
+    background-color: rgb(76, 46, 116);
+    width: 100%;
+    height: 100vh;
+}
+img {
+    height: 150px;
+    padding: 1rem;
+}
+</style>
