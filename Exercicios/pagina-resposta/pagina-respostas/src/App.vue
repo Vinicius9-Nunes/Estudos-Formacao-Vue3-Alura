822c392576edd9cc19775ca8c65d76e7788f3eb9
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-01 23:24:32 -0300

    Projeto estavel v1

--- Exercicios/pagina-resposta/pagina-respostas/src/App.vue
+++ Exercicios/pagina-resposta/pagina-respostas/src/App.vue
@@ -1,27 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <Cabecalho />
+  </div>
+  <div class="block"></div>
+  <div>
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <Resposta
+          v-for="(resposta, indice) in respostas"
+          v-bind:key="indice"
+          :resposta="resposta"
+          @aoCopiarResposta="PegarTextoAoCopiar"
+        />
+      </div>
+      <div class="column caixa-resposta">
+        <div class="caixa-interna">
+          <EdicaoResposta :textoAtual="textoAtual" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+  import { defineComponent, PropType } from "vue";
+  import Cabecalho from "./components/Cabecalho.vue";
+  import Resposta from "./components/Resposta.vue";
+  import EdicaoResposta from "./components/EdicaoResposta.vue";
+  import jsonRespostas from "./database/data.json";
+  import IResposta from "./Interfaces/IResposta";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  export default defineComponent({
+    name: "App",
+    components: {
+      Cabecalho,
+      Resposta,
+      EdicaoResposta,
+    },
+    data() {
+      return {
+        respostas: [] as PropType<IResposta[]>,
+        textoAtual: "",
+      };
+    },
+    methods: {
+      PegarTextoAoCopiar(texto: string) {
+        this.textoAtual = texto;
+        navigator.clipboard.writeText(texto);
+      },
+    },
+    mounted() {
+      let json = JSON.stringify(jsonRespostas);
+      this.respostas = JSON.parse(json).sort((a: IResposta, b: IResposta) => {
+        if(a.Titulo > b.Titulo) return 1
+        if(a.Titulo < b.Titulo) return -1
+        return 0
+      });
+    },
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: black;
+    background-color: rgba(219, 219, 219, 0.288);
+  }
+  .caixa-resposta {
+    display: flex;
+    justify-content: center;
+  }
+  .caixa-interna {
+    position: fixed;
+  }
 </style>
