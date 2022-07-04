e057cbcc28b34181d60f739f0318366be1c9e33a
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-04 20:47:48 -0300

    Ajuste definitivo commit errado

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <main  class="columns is-gapless is-multiline">
+  <main  class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral/>
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
-<<<<<<< HEAD
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoCriarTarefa="criarTarefa"/>
-=======
-    <div class="column is-three-quarter">
       <Formulario @aoSalvarTarefa="salvarTarefa"/>
->>>>>>> parent of 3c6daa2 (Ajuste para modo escuro)
       <div class="lista">
         <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
         <Box v-if="listaEstaVazia">
@@ -21,45 +16,49 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, transformVNodeArgs } from 'vue';
-import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from './Interfaces/ITarefa'
-import Box from './components/Box.vue';
-
-export default defineComponent({
+  import { defineComponent } from "vue";
+  import BarraLateral from "./components/BarraLateral.vue";
+  import Formulario from "./components/Formulario.vue";
+  import Tarefa from "./components/Tarefa.vue";
+  import ITarefa from "./Interfaces/ITarefa";
+  import Box from "./components/Box.vue";
+  export default defineComponent({
     name: "App",
     components: { BarraLateral, Formulario, Tarefa, Box },
-    data(){
+    data() {
       return {
-        tarefas: [] as ITarefa[]
-      }
+        tarefas: [] as ITarefa[],
+        modoEscuroAtivo: false,
+      };
     },
-    methods:{
-<<<<<<< HEAD
-      criarTarefa(descricaoTarefa: string){
-        const VALOR_INICIAL_TAREFA = 0
-        this.tarefas.push({descricao: descricaoTarefa, duracaoEmSegundos: VALOR_INICIAL_TAREFA})
+    methods: {
+      salvarTarefa(tarefa: ITarefa) {
+        this.tarefas.push(tarefa);
       },
-      trocarTema(modoEscuroAtivo: boolean){
-        this.modoEscuroAtivo = modoEscuroAtivo
-=======
-      salvarTarefa(tarefa: ITarefa){
-        this.tarefas.push(tarefa)
->>>>>>> parent of 3c6daa2 (Ajuste para modo escuro)
-      }
+      trocarTema(modoEscuroAtivo: boolean) {
+        this.modoEscuroAtivo = modoEscuroAtivo;
+      },
     },
-    computed:{
-      listaEstaVazia(): boolean{
-        return this.tarefas.length === 0
-      }
-    }
-});
+    computed: {
+      listaEstaVazia(): boolean {
+        return this.tarefas.length === 0;
+      },
+    },
+  });
 </script>
-
 <style>
-.lista{
-  padding: 1.25rem;
-}
+  .lista {
+    padding: 1.25rem;
+  }
+  main {
+    --bg-primario: #fff;
+    --texto-primario: #000;
+  }
+  main.modo-escuro {
+    --bg-primario: #2b2d42;
+    --texto-primario: #ddd;
+  }
+  .conteudo {
+    background-color: var(--bg-primario);
+  }
 </style>
