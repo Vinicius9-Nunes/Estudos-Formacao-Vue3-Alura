99d590e4b9b63a2bd08e3619a29c684f5de94fd0
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-04 21:53:24 -0300

    Nova Etapa - add vue router ao projeto

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <Box v-if="listaEstaVazia">
-          Você não esta muito produtivo hoje :(
-        </Box>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -18,38 +12,22 @@
 <script lang="ts">
   import { defineComponent } from "vue";
   import BarraLateral from "./components/BarraLateral.vue";
-  import Formulario from "./components/Formulario.vue";
-  import Tarefa from "./components/Tarefa.vue";
-  import ITarefa from "./Interfaces/ITarefa";
-  import Box from "./components/Box.vue";
   export default defineComponent({
     name: "App",
-    components: { BarraLateral, Formulario, Tarefa, Box },
+    components: { BarraLateral },
     data() {
       return {
-        tarefas: [] as ITarefa[],
         modoEscuroAtivo: false,
       };
     },
     methods: {
-      salvarTarefa(tarefa: ITarefa) {
-        this.tarefas.push(tarefa);
-      },
       trocarTema(modoEscuroAtivo: boolean) {
         this.modoEscuroAtivo = modoEscuroAtivo;
       },
-    },
-    computed: {
-      listaEstaVazia(): boolean {
-        return this.tarefas.length === 0;
-      },
-    },
+    }
   });
 </script>
 <style>
-  .lista {
-    padding: 1.25rem;
-  }
   main {
     --bg-primario: #fff;
     --texto-primario: #000;
