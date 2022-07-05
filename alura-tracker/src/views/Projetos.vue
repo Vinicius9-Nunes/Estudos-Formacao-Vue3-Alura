59cf46ecd68a774cd9fc6a2212a4140105a22f19
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-04 22:55:29 -0300

    Criando formulario e tabela de projetos

--- alura-tracker/src/views/Projetos.vue
+++ alura-tracker/src/views/Projetos.vue
@@ -15,6 +15,20 @@
         <button class="button" type="submit">Salvar</button>
       </div>
     </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
@@ -27,21 +41,24 @@
     data() {
       return {
         nomeDoProjeto: "",
-        projetos: [] as IProjeto[]
+        projetos: [] as IProjeto[],
       };
     },
     methods: {
       salvar() {
         const projeto: IProjeto = {
-            nome: this.nomeDoProjeto,
-            id: new Date().toISOString()
-        }
-        this.projetos.push(projeto)
-        this.nomeDoProjeto = ''
+          nome: this.nomeDoProjeto,
+          id: new Date().toISOString(),
+        };
+        this.projetos.push(projeto);
+        this.nomeDoProjeto = "";
       },
     },
   });
 </script>
 
-<style>
+<style scoped>
+  .projetos {
+    padding: 1.25rem;
+  }
 </style>
