5455efc008710b19f4eb9535e8414b594e756a97
Vinicius Nunes <vinicius.nunes@safeweb.com.br>
2022-07-04 22:39:29 -0300

    Criação inicial de projeto

---
+++ alura-tracker/src/views/Projetos.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+  import IProjeto from "@/Interfaces/IProjeto";
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: "ProjetosView",
+    data() {
+      return {
+        nomeDoProjeto: "",
+        projetos: [] as IProjeto[]
+      };
+    },
+    methods: {
+      salvar() {
+        const projeto: IProjeto = {
+            nome: this.nomeDoProjeto,
+            id: new Date().toISOString()
+        }
+        this.projetos.push(projeto)
+        this.nomeDoProjeto = ''
+      },
+    },
+  });
+</script>
+
+<style>
+</style>
